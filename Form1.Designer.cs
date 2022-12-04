f79ed2f875027ca0e4c7d75cd18f8767d282cd83
KevenFelixDaSilva <114019476+KevenFelixDaSilva@users.noreply.github.com>
2022-12-04 16:20:21 -0300

    alterando o desing

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,7 +35,6 @@ namespace Crud_1._0
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
@@ -96,15 +95,6 @@ namespace Crud_1._0
             this.label3.TabIndex = 6;
             this.label3.Text = "label3";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(110, 146);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "label4";
-            // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(151, 40);
@@ -142,7 +132,6 @@ namespace Crud_1._0
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -164,7 +153,6 @@ namespace Crud_1._0
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
